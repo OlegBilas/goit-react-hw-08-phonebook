ce76905beb0ce1da9fd2e45c43766607a1fdd4d3
Oleg Bilas <olegbilas@gmail.com>
2023-06-24 14:23:42 +0300

    finish

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,10 @@
-import { Helmet } from 'react-helmet';
 import { Text, Title, Wrapper } from './HomePage.styled';
 
 export default function HomePage() {
   return (
-    <>
-      <Helmet>
-        <title>Home</title>
-      </Helmet>
-      <Wrapper>
-        <Title>Phone Book</Title>
-        <Text>Welcome to your contacts' book for every day!</Text>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <Title>Phone Book</Title>
+      <Text>Welcome to your contacts' book for every day!</Text>
+    </Wrapper>
   );
 }
